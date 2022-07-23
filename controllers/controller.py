2e34482e66b0886bfb21bbdaac408fcf4dfa8677
Dorothea Hinrichs <dorothea.hinrichs@posteo.net>
2022-07-23 18:08:06 +0100

    can add a book, show a book, show all books

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template, request, redirect
 from app import app
-from models.orders import orders
+from models.orders import orders, add_new_book
+from models.order import Order
 
 @app.route('/orders')
 def index():
@@ -11,3 +12,16 @@ def singlebook(index):
   chosenbook = orders[int(index)]
   
   return render_template('order.html', order=chosenbook)
+
+@app.route('/order/new')
+def new():
+    return render_template('new.html', title='New', orders=orders)
+
+@app.route('/addbook', methods=['POST'])
+def addbook():
+    bookTitle = request.form['title']
+    bookAuthor = request.form['author']
+    bookGenre = request.form['genre']
+    newBook = Order(title=bookTitle, author=bookAuthor, genre=bookGenre)
+    add_new_book(newBook)
+    return redirect('/orders')
